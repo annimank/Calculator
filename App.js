b524a5ea2e6b54c1463d82ea184d13ddfffb1efa
annimank <mankki.anni@gmail.com>
2023-08-30 16:24:56 +0300

    Laskin historialla

--- App.js
+++ App.js
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, Button, Alert, TextInput, FlatList, TouchableOpacity } from 'react-native';
+import { StyleSheet, Text, View, TextInput, FlatList, ScrollView, Pressable, Image, KeyboardAvoidingView } from 'react-native';
 
 export default function App() {
   const [textOne, setTextOne] = useState("");
@@ -13,8 +13,10 @@ export default function App() {
     const res = num1 + num2;
     const operation = `${num1} + ${num2} = ${res}`;
     setCalculations(prevCalculations => [...prevCalculations, operation]);
-    Alert.alert("Result", res.toString());
+    //Alert.alert("Result", res.toString());
     console.log(res);
+    setTextOne("");
+    setTextTwo("");
   };
 
   const buttonTwo = () => {
@@ -23,93 +25,143 @@ export default function App() {
     const res = num1 - num2;
     const operation = `${num1} - ${num2} = ${res}`;
     setCalculations(prevCalculations => [...prevCalculations, operation]);
-    Alert.alert("Result", res.toString());
     console.log(res);
+    setTextOne("");
+    setTextTwo("");
+  };
+
+  const buttonThree = () => {
+    const num1 = parseInt(textOne);
+    const num2 = parseInt(textTwo);
+    const res = num1 * num2;
+    const operation = `${num1} * ${num2} = ${res}`;
+    setCalculations(prevCalculations => [...prevCalculations, operation]);
+    console.log(res);
+    setTextOne("");
+    setTextTwo("");
   };
 
   return (
-    <View style={styles.container}>
-      <View>
-        <Text style={styles.header}>I can do addition and subtraction</Text>
+    <KeyboardAvoidingView style={styles.container} behavior="padding">
+      <View style={styles.pic}>
+        <Image style={{ width: 250, height: 250 }} source={require('./assets/CalcLogo.png')} />
       </View>
       <View style={styles.textInput}>
-        <TextInput onChangeText={textOne => setTextOne(textOne)} value={textOne} placeholder="Enter number 1" keyboardType="numeric" />
-        <TextInput onChangeText={textTwo => setTextTwo(textTwo)} value={textTwo} placeholder="Enter number 2" keyboardType="numeric" />
+        <TextInput style={styles.textInputField} onChangeText={textOne => setTextOne(textOne)} value={textOne} placeholder="Enter number 1" keyboardType="numeric" />
+      </View>
+      <View style={styles.textInput}>
+        <TextInput style={styles.textInputField} onChangeText={textTwo => setTextTwo(textTwo)} value={textTwo} placeholder="Enter number 2" keyboardType="numeric" />
       </View>
       <View style={styles.buttonContainer}>
-        <TouchableOpacity style={styles.button} onPress={buttonOne}>
-          <Text style={styles.buttonText}>+</Text>
-        </TouchableOpacity>
-        <TouchableOpacity style={styles.button} onPress={buttonTwo}>
-          <Text style={styles.buttonText}>-</Text>
-        </TouchableOpacity>
+        <View>
+          <Pressable style={styles.button1} onPress={buttonOne}>
+            <Text style={styles.buttonText}>+</Text>
+          </Pressable>
+        </View>
+        <View>
+          <Pressable style={styles.button2} onPress={buttonTwo}>
+            <Text style={styles.buttonText}>-</Text>
+          </Pressable>
+        </View>
+        <View>
+          <Pressable style={styles.button3} onPress={buttonThree}>
+            <Text style={styles.buttonText}>*</Text>
+          </Pressable>
+        </View>
       </View>
-      <View style={styles.historyContainer}>
+      <View style={styles.resPic}>
+        <Image style={{ width: 150, height: 75 }} source={require('./assets/CalcResLogo.png')} />
+      </View>
+      <ScrollView style={styles.historyContainer}>
         <FlatList
           data={calculations}
-          renderItem={({ item }) => <Text>{item}</Text>}
+          renderItem={({ item }) => <Text style={styles.historyContainerText}>{item}</Text>}
           keyExtractor={(item, index) => index.toString()}
         />
-      </View>
+      </ScrollView>
       <StatusBar style="auto" />
-    </View>
+    </KeyboardAvoidingView>
   );
+  
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#C07156',
+    backgroundColor: '#285474',
     alignItems: 'center',
     justifyContent: 'center',
   },
-  header: {
-    backgroundColor: '#E8C3B6',
-    color: '#7D736C',
-    padding: 10,
-    marginBottom: 20,
-    marginTop: 20,
-    borderColor: '#EC4500',
-    borderWidth: 0,
-    alignItems: 'center',
-    width: 220,
-    borderRadius: 20,
-    textAlign: 'center',
-  },
   textInput: {
-    width: 220,
-    borderColor: '#EC6B00',
-    backgroundColor: '#E8C3B6',
-    borderWidth: 0,
-    marginBottom: 20,
-    marginTop: 20,
+    backgroundColor: '#1A4360',
+    margin: 10,
     padding: 5,
-    borderRadius: 20,
+    borderRadius: 2,
     alignItems: 'center',
+  },
+  textInputField: {
+    width: 120,
+    height: 50,
+    textAlign: 'center',
+    fontSize: 17,
+    color: '#8FAABD',
   },
   buttonContainer: {
     flexDirection: 'row',
-    justifyContent: 'space-between',
+    justifyContent: 'space-around',
     width: 60,
   },
-  button: {
-    backgroundColor: '#E8C3B6',
+  button1: {
+    backgroundColor: '#1A4360',
     width: 25,
     height: 25,
-    borderRadius: 60,
+    margin: 10,
+    borderRadius: 2,
+    marginRight: 30,
+  },
+  button2: {
+    backgroundColor: '#1A4360',
+    width: 25,
+    height: 25,
+    borderRadius: 2,
+    margin: 10,
+    marginRight: 30,
+    marginLeft: 30,
+  },
+  button3: {
+    backgroundColor: '#1A4360',
+    width: 25,
+    height: 25,
+    borderRadius: 2,
+    margin: 10,
+    marginLeft: 30,
   },
   buttonText: {
-    color: '#7D736C', 
+    color: '#8FAABD',
     fontSize: 20,
     textAlign: 'center',
-  },  
-  historyContainer: {
-    width: 220,
-    borderColor: '#EC6B00',
-    backgroundColor: '#E8C3B6',
-    borderWidth: 0,
-    padding: 10,
-    marginTop: 20,
-    borderRadius: 20,
   },
+  historyContainer: {
+    width: 200,
+    borderRadius: 2,
+    backgroundColor: '#1A4360',
+    margin: 20,
+    padding: 15,
+    borderRadius: 2,
+  },
+  historyContainerText: {
+    color: '#8FAABD',
+  },
+  pic: {
+    alignItems: 'center',
+    marginTop: 10,
+  },
+  resPic: {
+    alignItems: 'center',
+    width: 10,
+    height: 5,
+    marginBottom: 50,
+    marginTop: 20,
+  },
+
 });
